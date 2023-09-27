b3b242f2b61e16829a74b070fade4b5a29b083ec
arsiamallahbinhaq <arsiamallahbinhaq@gmail.com>
2023-09-27 09:46:40 +0700

    upload

---
+++ src/handler.js
@@ -0,0 +1,106 @@
+const books = require('./books');
+const {nanoid} = require('nanoid');
+
+const addBookHandler = (request, h) => {
+    const {
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading,
+    } = request.payload;
+    if (!name) {
+        return h.response({
+            status: 'fail',
+            message:
+            'Gagal menambahkan buku. Mohon isi nama buku',
+        }).code(400);
+    }
+
+    if (readPage > pageCount) {
+        return h.response({
+            status: 'fail',
+            message: 
+            'readPage tidak boleh lebih besar dari pageCount',
+        }).code(400);
+    }
+
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+    const finished = pageCount === readPage;
+    const newBook = {
+        id,
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        finished,
+        reading,
+        insertedAt,
+        updatedAt,
+    };
+    books.push(newBook);
+    const isSuccess = books.filter((book) => book.id === id).length > 0;
+    if (isSuccess) {
+        const response = h.response ({
+            status: 'success',
+            message: 'Buku berhasil ditambahkan',
+            data: {
+                bookId: id,
+            },
+        });
+        response.code(201);
+        return response;
+    }
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku gagal ditambahkan',
+    });
+    response.code(500);
+    return response;
+
+};
+
+const getAllBooksHandler = (request, h) => {
+    const {query} = request;
+
+    let filteredBooks = [...books];
+
+    // Filter berdasarkan nama
+    if (query.name) {
+        const searchName = query.name.toLowerCase();
+        filteredBooks = filteredBooks.filter((book) => book.name.toLowerCase().includes(searchName),
+        );
+    }
+    // Filter berdasarkan status reading
+    if (query.reading === '0' || query.reading === '1') {
+        const readingValue = query.reading === '1';
+        filteredBooks = filteredBooks.filter((book) => book.reading === readingValue,
+        );
+    }
+        // Filter berdasarkan status finished
+    if (query.finished === '0' || query.finished === '1') {
+        const finishedValue = query.finished === '1';
+        filteredBooks = filteredBooks.filter((book) => book.finished === finishedValue,
+        );
+    }
+
+    const response = {
+        status: 'success',
+        data: {
+            books: filteredBooks.map((book) => ({
+                id: book.id,
+                name: book.name,
+                publisher: book.publisher,
+            })),
+        },
+    };
+    return h.response(response).code(200);
+};
